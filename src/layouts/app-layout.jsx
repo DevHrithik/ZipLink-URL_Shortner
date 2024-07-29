77848e474edc0996a1f63880b86c4efef8f6a842
Hrithik <hrithikroy1854@gmail.com>
2024-07-29 20:33:33 +0530

    basic setup complete
    pages and routes setup

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,14 @@
+const AppLayout = () => {
+  return (
+    <div>
+      AppLayout
+      <main>
+        {/* <NavBar /> */}
+        {/* Body */}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default AppLayout;
