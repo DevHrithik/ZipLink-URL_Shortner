d81548e3d847ac01ed984db5dcc79a35b11d209b
Hrithik <hrithikroy1854@gmail.com>
2024-07-31 17:11:55 +0530

    dynamic header and logout logic

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,44 +10,62 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu.jsx";
 import { LinkIcon, LogOut } from "lucide-react";
+import { UrlState } from "../../context.jsx";
+import useFetch from "@/hooks/use-fetch.js";
+import { BarLoader } from "react-spinners";
+import { logout } from "@/utils/apiAuth.js";
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = false;
+  const { user, fetchUser } = UrlState();
+  const { loading, fn: fnLogout } = useFetch(logout);
   return (
-    <nav className="flex justify-between items-center py-4">
-      <Link className="text-3xl gradient-text" to="/">
-        <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-          ZipLink
-        </h1>
-      </Link>
-      <div className="pt-2">
-        {!user ? (
-          <Button onClick={() => navigate("/auth")}>Login</Button>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger clasName="w-10 rounded-full overflow-hidden">
-              <Avatar>
-                <AvatarImage src="https://github.com/DevHrithik.png" />
-                <AvatarFallback>HR</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Hrithik Roy</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <LinkIcon classNAme="mr-2 h-4 w-4" />
-                <span>My Links</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="bg-red-400">
-                <LogOut classNAme="mr-2 h-4 w-4" />
-                <span>Logout</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-    </nav>
+    <>
+      <nav className="flex justify-between items-center py-4">
+        <Link className="text-3xl gradient-text" to="/">
+          <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+            ZipLink
+          </h1>
+        </Link>
+        <div className="pt-2">
+          {!user ? (
+            <Button onClick={() => navigate("/auth")}>Login</Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger clasName="w-10 rounded-full overflow-hidden">
+                <Avatar>
+                  <AvatarImage src={user?.user_metadata?.profile_pic} />
+                  <AvatarFallback>NA</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  {user?.user_metadata?.name}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <LinkIcon classNAme="mr-2 h-4 w-4" />
+                  <span>My Links</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => {
+                    fnLogout().then(() => {
+                      fetchUser();
+                      navigate("/auth");
+                    });
+                  }}
+                  className="text-red-400"
+                >
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Logout</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>{" "}
+      {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+    </>
   );
 };
 
